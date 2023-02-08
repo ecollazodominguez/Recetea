ad72fe5be808a6ac6c8d1bd3687cbeed6f78d1a7
Eduardo Collazo <ecollazodominguez@gmail.com>
2023-02-08 18:01:48 +0100

    SCSS for Header done

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,22 +1,29 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useWindowDimensions } from "../../hooks/useWindowDimensions";
+import { Console, log } from "console";
 
-const islogged = false; //testing purposes
-interface IProps {
-  firstLinkName: string;
-  firstURL: string;
+const islogged = false; // Testing purposes
+
+// Interface for props for component NavLinks
+interface INavLinksProps {
+  firstLinkName: string; // 1st Link Tag name
+  firstURL: string; // 1st Link Tag where to go: "/something"
   secondLinkName: string;
   secondURL: string;
 }
-interface IToggle {
-  isToggle: boolean;
+
+//Iinterface for props for toggle components
+interface IToggleProps {
+  isToggleNavMenu: boolean; // Is Expanded?
+  setIsToggleNavMenu: Function; // Setter
 }
 
 export const Header = () => {
   return (
     <header className="Header">
       <TopHeader />
+      <BottomHeader />
     </header>
   );
 };
@@ -24,30 +31,16 @@ export const Header = () => {
 const TopHeader = () => {
   const { height, width } = useWindowDimensions();
   return (
-    <>
+    <section className="Header-top">
       <h1 className="Header-h1">Recetea</h1>
       {width > 1000 ? <DesktopNavMenu /> : <ToggleNavMenu />}
-      <BottomHeader />
-    </>
-  );
-};
-
-const BottomHeader = () => {
-  return (
-    <nav className="Header-nav">
-      <Link to="/" className="Header-link">
-        Recetas Veganas
-      </Link>
-      <Link to="/" className="Header-link">
-        Recetas Fáciles
-      </Link>
-    </nav>
+    </section>
   );
 };
 
 const DesktopNavMenu = () => {
   return (
-    <nav className="Header-nav">
+    <nav className="Header-desktopNav">
       {islogged ? (
         <NavLinks
           firstLinkName="Crear Receta"
@@ -72,7 +65,7 @@ const NavLinks = ({
   firstURL,
   secondLinkName,
   secondURL,
-}: IProps) => {
+}: INavLinksProps) => {
   return (
     <>
       <Link to={firstURL} className="Header-link">
@@ -86,37 +79,94 @@ const NavLinks = ({
 };
 
 const ToggleNavMenu = () => {
-  const [isToggleNavMenu, setIsToggleNavMenu] = useState(false);
-  const handleDropdown = () => {
+  // Make a boolean state to check if the menu is expanded or no
+  const [isToggleNavMenu, setIsToggleNavMenu] = useState<boolean>(false);
+
+  // Function on click to change boolean value
+  const handleDropdown = (): void => {
     setIsToggleNavMenu(!isToggleNavMenu);
   };
+
   return (
     <>
       <button className="Header-button" onClick={handleDropdown}>
         <img className="Header-img" src="menu.svg" alt="Menu SVG" />
       </button>
-      <NavMenuContent isToggle={isToggleNavMenu} />
+      <NavMenuContent
+        isToggleNavMenu={isToggleNavMenu}
+        setIsToggleNavMenu={setIsToggleNavMenu}
+      />
     </>
   );
 };
 
-const NavMenuContent = ({ isToggle }: IToggle) => {
+const NavMenuContent = ({
+  isToggleNavMenu,
+  setIsToggleNavMenu,
+}: IToggleProps) => {
+  // Referencing the article on this component (The menu we wanto to expand and collapse)
+  const node = useRef<HTMLElement>(null);
+
+  // Function to check if the node referencing (article) is the same as what the mouse clicked (e.target)
+  const clickOutsideDropdown = (e: Event): void => {
+    // If it's not the case we change the value to false and the menu will collapse
+    if (node.current && !node.current.contains(e.target as Node)) {
+      setIsToggleNavMenu(false);
+    }
+    // If they are the same (meaning you clicked the article itself), menu will still be expanded
+  };
+
+  // UseEffect to use the previous function. Checks every time you click something.
+  // Everytime the boolean changes, remove the old listener and make new ones.
+  useEffect(() => {
+    document.addEventListener("touchmove", clickOutsideDropdown);
+    document.addEventListener("mousedown", clickOutsideDropdown);
+    return () => {
+      document.removeEventListener("touchmove", clickOutsideDropdown);
+      document.removeEventListener("mousedown", clickOutsideDropdown);
+    };
+  }, [isToggleNavMenu]);
+
+  // We added corresponding className depending if it is expanded or not and make the transition on SCSS.
   return (
     <section
-      className={isToggle ? "Header-toggle is-expanded" : "Header-toggle"}
+      className={
+        isToggleNavMenu ? "Header-toggle is-expanded" : "Header-toggle"
+      }
     >
-      <h2 className="Header-h2">Receteado</h2>
-      <nav className="Header-nav">
-        <Link to="/" className="Header-link">
-          Recetas
-        </Link>
-        <Link to="/" className="Header-link">
-          Recetas
-        </Link>
-        <Link to="/" className="Header-link">
-          Recetas
-        </Link>
-      </nav>
+      <article
+        ref={node}
+        className={
+          isToggleNavMenu
+            ? "Header-toggleArticle is-expanded"
+            : "Header-toggleArticle"
+        }
+      >
+        <h2 className="Header-h2">Receteado</h2>
+        <nav className="Header-navToggle">
+          <Link to="/" className="Header-linkToggle">
+            Recetas Veganas
+          </Link>
+          <Link to="/" className="Header-linkToggle">
+            Recetas de Postre
+          </Link>
+          <Link to="/" className="Header-linkToggle">
+            Recetas Fáciles
+          </Link>
+        </nav>
+      </article>
     </section>
+  );
+};
+const BottomHeader = () => {
+  return (
+    <nav className="Header-nav">
+      <Link to="/" className="Header-link">
+        Recetas Veganas
+      </Link>
+      <Link to="/" className="Header-link">
+        Recetas Fáciles
+      </Link>
+    </nav>
   );
 };
--- src/hooks/useWindowDimensions.ts
+++ src/hooks/useWindowDimensions.ts
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import internal from "stream";
 
 interface IDimensions {
   width: number;
